1da61524db1f54b8f3d41b49311880c0b4c5147c
Edward Z. Yang <ezyang@fb.com>
2019-08-05 11:20:13 -0700

    Update conda packaging for master nightlies
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- packaging/conda/build_vision.sh
+++ packaging/conda/build_vision.sh
@@ -12,12 +12,12 @@ retry () {
 
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters. Pass cuda version"
-    echo "CUDA version should be M.m with no dot, e.g. '8.0' or 'cpu'"
+    echo "CUDA version should be M.m with dot, e.g. '8.0' or 'cpu'"
     exit 1
 fi
 desired_cuda="$1"
 
-export TORCHVISION_BUILD_VERSION="0.3.0"
+export TORCHVISION_BUILD_VERSION="0.4.0.dev$(date "+%Y%m%d")"
 export TORCHVISION_BUILD_NUMBER=1
 
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
@@ -34,13 +34,16 @@ else
     vision_rootdir="$(pwd)/torchvision-src"
 fi
 
+if [[ "$BRANCH" == "" ]]; then
+  BRANCH=master
+fi
+
 if [[ ! -d "$vision_rootdir" ]]; then
     rm -rf "$vision_rootdir"
-    git clone "https://github.com/pytorch/vision" "$vision_rootdir"
-    pushd "$vision_rootdir"
-    git checkout v$TORCHVISION_BUILD_VERSION
-    popd
+    git clone "https://github.com/pytorch/vision" "$vision_rootdir" -b "$BRANCH"
 fi
+
+export TORCHVISION_GITHUB_ROOT_DIR="$vision_rootdir"
 
 cd "$SOURCE_DIR"
 
@@ -59,28 +62,41 @@ if [[ "$OSTYPE" == "msys" ]]; then
     retry conda install -yq conda-build
 fi
 
-ANACONDA_USER=pytorch
+ANACONDA_USER=pytorch-nightly
 conda config --set anaconda_upload no
 
 
-export TORCHVISION_PACKAGE_SUFFIX=""
 if [[ "$desired_cuda" == 'cpu' ]]; then
+    export CONDA_CPU_ONLY_FEATURE="    - cpuonly # [not osx]"
     export CONDA_CUDATOOLKIT_CONSTRAINT=""
     export CUDA_VERSION="None"
-    if [[ "$OSTYPE" != "darwin"* ]]; then
-        export TORCHVISION_PACKAGE_SUFFIX="-cpu"
-    fi
+    export VERSION_MATCH="*+cpu"
 else
     . ./switch_cuda_version.sh $desired_cuda
+    export CONDA_CPU_ONLY_FEATURE=""
     if [[ "$desired_cuda" == "10.0" ]]; then
-	export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=10.0,<10.1 # [not osx]"
+        export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=10.0,<10.1 # [not osx]"
+        export VERSION_MATCH="*"
     elif [[ "$desired_cuda" == "9.0" ]]; then
-	export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=9.0,<9.1 # [not osx]"
+        export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=9.0,<9.1 # [not osx]"
+        export VERSION_MATCH="*+cu90"
+    elif [[ "$desired_cuda" == "9.2" ]]; then
+        export CONDA_CUDATOOLKIT_CONSTRAINT="    - cudatoolkit >=9.2,<9.3 # [not osx]"
+        export VERSION_MATCH="*+cu92"
     else
-	echo "unhandled desired_cuda: $desired_cuda"
-	exit 1
+        echo "unhandled desired_cuda: $desired_cuda"
+        exit 1
     fi
 fi
+
+
+
+LATEST_PYTORCH_NIGHTLY_VERSION=$(conda search --json "pytorch[channel=$ANACONDA_USER]==$VERSION_MATCH" | python "$SOURCE_DIR/get-latest.py")
+# NB: We must provide +cu90, etc. in the constraint here
+# This relies on Conda NOT respecting PEP 440 and letting
+# us do wildcards with local version specifiers, which empirically
+# seemed to work on 4.7.10
+export CONDA_PYTORCH_CONSTRAINT="    - pytorch ==${LATEST_PYTORCH_NIGHTLY_VERSION}"
 
 if [[ "$OSTYPE" == "msys" ]]; then
     time conda build -c $ANACONDA_USER --no-anaconda-upload vs2017
